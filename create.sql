24f3717faf947bb678737a8c34f01584fe93b14e
Adwaith-RK <adwaith_b190808cs@nitc.ac.in>
2021-06-19 21:34:42 +0530

    test 2

--- create.sql
+++ create.sql
@@ -1,26 +1,22 @@
-create table publisher 
-(
+create table publisher (
 	id integer primary key autoincrement,
 	name text,
 	country text
 );
 
-create table books
-(
+create table books(
 	id integer primary key autoincrement,
 	title text,
 	publisher integer,
 	foreign key(publisher) references publisher(id)
 );
 
-create table subjects
-(
+create table subjects(
 	id integer primary key autoincrement,
 	name text
 );
 
-create table books_subjects
-(
+create table books_subjects (
 	foreign key(books) references books(id),
 	foreign key(subjects) references subjects(id)
 );
